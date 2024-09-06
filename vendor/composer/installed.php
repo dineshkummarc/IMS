9c46bf6229fb6d29f917e7ecc42b560439d69621
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-06 12:46:13 +0530

    updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '88b11343426e68914a5171ab1fa6a4cc350ae136',
+        'reference' => 'a0028f26d5c2832992f7ef0505d87529552eb83f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '88b11343426e68914a5171ab1fa6a4cc350ae136',
+            'reference' => 'a0028f26d5c2832992f7ef0505d87529552eb83f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
